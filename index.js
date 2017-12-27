0dabf40eac96fe0cfc50bf05765c1de8db0c5e75
wemlion <angusfu1126@qq.com>
2017-12-27 23:59:35 +0800

    glob support

--- index.js
+++ index.js
@@ -1,43 +1,75 @@
-const { basename } = require('path')
+const { basename, resolve, dirname } = require('path')
 const { readFile } = require('fs-extra')
+
+const glob = require('glob')
 const { compiler } = require('vueify')
 const { createFilter } = require('rollup-pluginutils')
 
 const wrapCompiled = function ({ result, name }) {
-  return `export default (function (module) {
+  return `(function (module) {
   ${result}
   return module.exports;
 })({exports: {
-  name: ${name}
-}});`
+  name: ${JSON.stringify(name)}
+}})`
 }
 
 module.exports = ({ include, exclude } = {}) => {
   const filter = createFilter(include, exclude)
+  const map = {}
 
   return {
-    load (id) {
-      if (!filter(id) || !/.svg$/.test(id)) {
+    name: 'rollup-plugin-svg-vue',
+
+    resolveId (importee, importer) {
+      if (!filter(importee) || !/.svg$/.test(importee)) {
         return
       }
 
-      return readFile(id, 'utf-8')
-        .then(source => {
-          if (source.trim() === '') {
-            throw new Error(`Empty svg: ${id}`)
-          }
-          return source
-        })
-        .then(source => `<template>${source}</template>`)
-        .then(template => compilePromise(template, id))
-        .then(result => {
-          return wrapCompiled({
-            result,
-            name: basename(id, '.svg')
-          })
-        })
+      const id = `$$SVG__${importee}`
+      map[id] = { importee, importer }
+      return id
+    },
+
+    load (id) {
+      if (!map[id]) {
+        return
+      }
+
+      const { importee, importer } = map[id]
+      const cwd = dirname(importer)
+
+      if (!/\*/.test(importee)) {
+        const file = resolve(cwd, importee)
+        return svg2vue(file)
+          .then(code => `export default ${code}`)
+      }
+
+      const files = glob.sync(importee, { cwd })
+      return Promise.all(
+        files.map(file => svg2vue(resolve(cwd, file)))
+      )
+      .then(codes => `export default [\n${codes.join('\n,')}\n]`)
     }
   }
+}
+
+function svg2vue (file) {
+  return readFile(file, 'utf-8')
+    .then(source => {
+      if (source.trim() === '') {
+        throw new Error(`Empty svg: ${file}`)
+      }
+      return source
+    })
+    .then(source => `<template>${source}</template>`)
+    .then(template => compilePromise(template, file))
+    .then(result => {
+      return wrapCompiled({
+        result,
+        name: basename(file, '.svg')
+      })
+    })
 }
 
 function compilePromise (code, path) {
