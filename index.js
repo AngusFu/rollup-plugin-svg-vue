2c8b50f22d773dca9155cb19218761d07eb8ed83
文蔺 <AngusFu@users.noreply.github.com>
2017-12-28 14:56:54 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-const { basename, resolve, dirname } = require('path')
-const { readFile } = require('fs-extra')
+const {
+  readFile
+} = require('fs-extra')
+const {
+  basename,
+  resolve,
+  dirname,
+  isAbsolutePath,
+  isRelativePath
+} = require('x-path')
 
 const glob = require('glob')
 const { compiler } = require('vueify')
 const { createFilter } = require('rollup-pluginutils')
+const findNpmPrefix = require('find-npm-prefix')
 
 const wrapCompiled = function ({ result, name }) {
   return `(function (module) {
@@ -26,9 +35,15 @@ module.exports = ({ include, exclude } = {}) => {
         return
       }
 
-      const id = `$$SVG__${importee}`
-      map[id] = { importee, importer }
-      return id
+      return ensureNpmPrefix().then(npmPrefix => {
+        if (!isAbsolutePath(importee) && !isRelativePath(importee)) {
+          importee = resolve(npmPrefix, 'node_modules', importee)
+        }
+
+        const id = `$$SVG__${importee}`
+        map[id] = { importee, importer }
+        return id
+      })
     },
 
     load (id) {
@@ -46,10 +61,11 @@ module.exports = ({ include, exclude } = {}) => {
       }
 
       const files = glob.sync(importee, { cwd })
+
       return Promise.all(
-        files.map(file => svg2vue(resolve(cwd, file)))
-      )
-      .then(codes => `export default [\n${codes.join('\n,')}\n]`)
+          files.map(file => svg2vue(resolve(cwd, file)))
+        )
+        .then(codes => `export default [\n${codes.join('\n,')}\n]`)
     }
   }
 }
@@ -81,5 +97,16 @@ function compilePromise (code, path) {
         resolve(result)
       }
     })
+  })
+}
+
+let npmPrefix = null
+function ensureNpmPrefix () {
+  if (npmPrefix) {
+    return Promise.resolve(npmPrefix)
+  }
+  return findNpmPrefix(process.cwd()).then(prefix => {
+    npmPrefix = prefix
+    return prefix
   })
 }
